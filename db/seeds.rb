c7bb96e2aabd07347c129708122c0b99c4b5f628
gercks <elizagercke@gmail.com>
2018-01-18 16:12:05 -0500

    Update seed file to no longer require pry

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,15 +11,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 require 'csv'
-require 'pry'
 
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'Queeresources-Spreadsheet1.csv'))
 file_path = File.path(Rails.root.join('lib', 'seeds', 'Queeresources-Spreadsheet1.csv'))
-# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-# puts csv
-# csv = CSV.parse(csv_text, :headers => true, :encoding => 'iso-8859-1:utf-8')
 CSV.foreach(file_path, headers: true, encoding: 'ISO-8859-1') do |row|
-# csv.each do |row|
   q = Queeresource.new
   q.user_id = 1
   q.name = row['Name']
