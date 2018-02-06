6eb8f7c3f7ab3e99af1d156c7ba48442470ef91b
gercks <elizagercke@gmail.com>
2018-02-06 10:17:06 -0500

    Add editable to comment & queeresource serializers

--- app/serializers/queeresource_serializer.rb
+++ app/serializers/queeresource_serializer.rb
@@ -1,4 +1,11 @@
 class QueeresourceSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :location, :website, :orgtype, :popfocus
   has_one :user
+  def editable
+    scope == object.user
+  end
+
+  def comments
+    object.comments.pluck(:id)
+  end
 end
