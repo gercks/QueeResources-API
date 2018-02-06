529e73f58ceb89af7942e46b974f1ef050eb108a
gercks <elizagercke@gmail.com>
2018-02-06 10:59:41 -0500

    Comment out editable for test

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -2,11 +2,11 @@ class CommentSerializer < ActiveModel::Serializer
   attributes :id, :text
   has_one :user
   has_one :queeresource
-  def editable
-    scope == object.user
-  end
-
-  def queeresource
-    object.queeresource.id
-  end
+  # def editable
+  #   scope == object.user
+  # end
+  #
+  # def queeresource
+  #   object.queeresource.id
+  # end
 end
